6ab9fa06c68b9dd9cad7b496435fa658f2e57eb9
Warakom <warakom247365@gmail.com>
2021-12-18 23:03:08 +0100

    Screen Wraparound

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    private void ProcessInput()
+    {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             var touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
@@ -28,6 +34,29 @@ public class PlayerMovement : MonoBehaviour
         {
             movementDirection = Vector3.zero;
         }
+    }
+
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+        var viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+        if(viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if(viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+        if(viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+        transform.position = newPosition;
     }
 
     private void FixedUpdate()
